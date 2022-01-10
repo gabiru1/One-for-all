7d88eb8e36233a254aa281e46d248f0f971c0e03
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2022-01-10 16:20:21 -0300

    tabela de planos

--- desafio1.sql
+++ desafio1.sql
@@ -2,6 +2,13 @@ DROP DATABASE IF EXISTS SpotifyClone;
 CREATE DATABASE IF NOT EXISTS SpotifyClone;
 USE SpotifyClone;
 
+CREATE TABLE plans(
+  plan_id INT NOT NULL AUTO_INCREMENT,
+  plan_name VARCHAR(13) NOT NULL,
+  plan_value DECIMAL(3,2) NOT NULL,
+  PRIMARY KEY (plan_id)
+) engine=InnoDB;
+
 CREATE TABLE users (
   user_id INT NOT NULL AUTO_INCREMENT,
   user_name VARCHAR(25) NOT NULL DEFAULT 'nenhum nome inserido',
