cd413a2be8a226fa75ea08f6934bbd7d3cc5596c
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2022-01-11 19:09:04 -0300

    desafio 6 teste

--- desafio6.sql
+++ desafio6.sql
@@ -2,7 +2,7 @@ SELECT
 	MIN(plans.plan_value) AS 'faturamento_minimo',
     MAX(plans.plan_value) AS 'faturamento_maximo',
     FORMAT(AVG(plans.plan_value), 2) AS 'faturamento_medio',
-    SUM(plans.plan_value) AS 'faturamento_total'
+    CAST(SUM(plans.plan_value) AS DECIMAL(4,2)) AS 'faturamento_total'
 FROM
 	SpotifyClone.plans as plans,
 	SpotifyClone.users as users
