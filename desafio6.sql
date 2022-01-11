105111f650df730641040aea676bd6a214e10596
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2022-01-11 18:23:38 -0300

    desafio 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,6 +1,10 @@
 SELECT
-	MIN(SpotifyClone.plans.plan_value) AS 'faturamento_minimo',
-    MAX(SpotifyClone.plans.plan_value) AS 'faturamento_maximo',
-    'faturamento_medio',
-    'faturamento_total'
-FROM SpotifyClone.plans
+	MIN(plans.plan_value) AS 'faturamento_minimo',
+    MAX(plans.plan_value) AS 'faturamento_maximo',
+    FORMAT(AVG(plans.plan_value), 2) AS 'faturamento_medio',
+    SUM(plans.plan_value) AS 'faturamento_total'
+FROM
+	SpotifyClone.plans as plans,
+	SpotifyClone.users as users
+WHERE
+	users.plan_id = plans.plan_id
