0c823f77cb23a7d6e52983d8b8bf50cbc14e4241
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2022-01-11 15:50:56 -0300

    desafio 4

---
+++ desafio4.sql
@@ -0,0 +1,9 @@
+SELECT user.user_name AS usuario, 
+CASE
+	WHEN MAX(YEAR(his.history_date)) = 2021 THEN 'Usuário ativo'
+  ELSE 'Usuário inativo'
+END AS condicao_usuario
+FROM SpotifyClone.users AS user, SpotifyClone.history AS his
+WHERE user.user_id = his.user_id
+GROUP BY user.user_name
+ORDER BY user.user_name;
