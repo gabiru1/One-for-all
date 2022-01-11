17542e29c4b25ee077d3c7a983a03ec1c909b768
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2022-01-11 16:31:30 -0300

    referencia

--- desafio5.sql
+++ desafio5.sql
@@ -8,3 +8,6 @@ WHERE SpotifyClone.track.track_id = SpotifyClone.`history`.track_id
 GROUP BY SpotifyClone.track.track_name
 ORDER BY reproducoes DESC, SpotifyClone.track.track_name
 LIMIT 2;
+
+-- Fiz sem alias para fins didaticos.
+-- Creditos ao meu amigo Rafael (https://github.com/perchespierri) que me ajudou a entender
